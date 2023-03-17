39332221e0ddfe668ce32e65531789e8463789e4
Dami Adenugba <carlos.adenugba.836@my.csun.edu>
2023-03-17 10:17:17 -0700

    final program

--- src/MelodyQueue.java
+++ src/MelodyQueue.java
@@ -12,18 +12,21 @@ public class MelodyQueue {
 	}
 	
 	public void enqueue(Object item) {
-		if(front == null)							// checks to see if queue is empty
+		if(isEmpty()) {								// checks to see if queue is empty
 			front = new Node(item);
+			back = front;
+		}
 		else {
 			back = front;
 			while(back.getNext() != null)			// traverses the queue with node 'back'
 				back = back.getNext();
-			back.setNext(new Node(item));			// points the 'back' to the new node
+			back.setNext(new Node(item));			// points the last node in the queue to the new node
+			back = back.getNext();					// points 'back' to the new node
 		}
 	}
 	
 	public Object dequeue() {
-		if(front == null) 							// checks to see if queue is empty
+		if(isEmpty()) 								// checks to see if queue is empty
 			System.out.println("Queue is empty");
 		else {
 			Node temp = front;						// retrieves the node at the front
@@ -41,11 +44,27 @@ public class MelodyQueue {
 		double songDuration = 0;
 		for(Node curr = front; curr != null; curr = curr.getNext())
 			songDuration += ((Note)curr.getItem()).getDuration();
-		return songDuration;
+		return songDuration + timeRepeat();
 	}
 	
 	public double timeRepeat() {
-		return 0;
+		double repeatDuration = 0;
+		Node curr = front;
+		Note note = (Note)curr.getItem();
+		
+		while(curr.getNext() != null) {
+			if(note.isRepeat()) {
+				do {
+					repeatDuration += ((Note)curr.getItem()).getDuration();
+					curr = curr.getNext();
+					note = (Note)curr.getItem();
+				}while(!note.isRepeat());
+				repeatDuration += ((Note)curr.getItem()).getDuration();
+			}	
+			curr = curr.getNext();
+			note = (Note)curr.getItem();
+		}	
+		return repeatDuration;
 	}
 	
 	public int size() {
@@ -63,23 +82,62 @@ public class MelodyQueue {
 	}
 	
 	public void tempoChange(double tempo) {
-		
+		double newDuration = 0;
+		for(Node curr = front; curr != null; curr = curr.getNext()) {
+			newDuration = ((Note)curr.getItem()).getDuration() * tempo;
+			((Note)curr.getItem()).setDuration(newDuration);
+		}
 	}
 	
 	public void playRepeat() {
-		
+		for(Node curr = front; curr != null; curr = curr.getNext()) {
+			Note note = ((Note)curr.getItem());
+			note.play();
+		}
 	}
 	
 	public void appendMelody(MelodyQueue other) {
-		
+		this.back.setNext(other.front);
+		this.back = other.back;
 	}
 	
 	public MelodyQueue reverseMelody() {
-		return null;
+		NoteStack stack = new NoteStack();
+		for(Node curr = front; curr != null; curr = curr.getNext())
+			stack.push(curr.getItem());
+		
+		MelodyQueue reverse = new MelodyQueue();
+		for(Node curr = front; curr != null; curr = curr.getNext())
+			reverse.enqueue(stack.pop());
+		return reverse;
 	}
 	
 	public void play() {
-		
+		Node curr = front;
+		boolean isRepeated;
+		MelodyQueue temp = new MelodyQueue();
+		while(curr != null) {
+			Note note = ((Note)curr.getItem());
+			isRepeated = note.isRepeat();
+			if(isRepeated) {
+				do {
+					temp.enqueue(note);
+					note.play();
+					curr = curr.getNext();
+					note = ((Note)curr.getItem());
+					isRepeated = note.isRepeat();
+				}while(!isRepeated);
+				
+				temp.enqueue(note);
+				note.play();
+				temp.playRepeat();
+				curr = curr.getNext();
+			}
+			else {
+				note.play();
+				curr = curr.getNext();
+			}
+		}
 	}
 	
 }
